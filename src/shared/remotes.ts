001df152f116cd5131406335765ed7db28df1c5c
tairasoul <mjrcoetzee+1@outlook.com>
2024-02-08 21:57:55 +0200

    first arg is depth

--- src/shared/remotes.ts
+++ src/shared/remotes.ts
@@ -3,7 +3,7 @@ import { Server, createRemotes, remote } from "@rbxts/remo";
 export const remotes = createRemotes(
     {
         generateRoom: remote<Server>(),
-        generateRoomWithDepth: remote<Server, [number]>(),
+        generateRoomWithDepth: remote<Server, [depth: number]>(),
         clearTiles: remote<Server>()
     }
 )
