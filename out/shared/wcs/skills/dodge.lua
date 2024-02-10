ab3b1911ab0536d193050aee34cbc3948ed0d6b5
tairasoul <mjrcoetzee+1@outlook.com>
2024-02-10 21:29:02 +0200

    e

--- out/shared/wcs/skills/dodge.lua
+++ out/shared/wcs/skills/dodge.lua
@@ -25,32 +25,6 @@ do
 		super.constructor(self, ...)
 	end
 	function Dodge:OnStartServer()
-		--[[
-			const characterModel = this.Character.Instance as Model;
-			const primary = characterModel.WaitForChild("HumanoidRootPart") as BasePart;
-			const humanoid = characterModel.WaitForChild("Humanoid") as Humanoid;
-			const system = healthRegistry.addHumanoid(humanoid);
-			system.canTakeDamage = false;
-			primary.Anchored = true;
-			const lookVector = primary.CFrame.LookVector;
-			const params = new RaycastParams();
-			params.FilterType = Enum.RaycastFilterType.Exclude;
-			params.FilterDescendantsInstances = characterModel.GetDescendants();
-			let distance = new Vector3(-12, 0, -12);
-			const raycastResult = Workspace.Raycast(characterModel.GetPivot().Position, lookVector.mul(distance), params);
-			if (raycastResult !== undefined) {
-			distance = new Vector3(-raycastResult.Distance, 0, -raycastResult.Distance);
-			}
-			const tweenI = new TweenInfo(0.3, Enum.EasingStyle.Sine, Enum.EasingDirection.Out);
-			const tween = TweenService.Create(primary, tweenI, {CFrame: primary.CFrame.add(lookVector.mul(distance))});
-			tween.Play();
-			tween.Completed.Once(() => {
-			primary.Anchored = false
-			task.wait(0.1);
-			system.canTakeDamage = true;
-			});
-			this.ApplyCooldown(1);
-		]]
 		local characterModel = self.Character.Instance
 		local humanoid = characterModel:WaitForChild("Humanoid")
 		local system = healthRegistry:addHumanoid(humanoid)
